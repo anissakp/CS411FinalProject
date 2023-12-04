91b458c885ea0c21ac38a7eea26cfc789941c4f4
Ana <anasof@bu.edu>
2023-12-04 14:40:58 -0500

    "updating location models"

--- server/src/models/locationsModel.js
+++ server/src/models/locationsModel.js
@@ -4,10 +4,15 @@ const locationSchema = new mongoose.Schema({
     name: {
         type: String
     },
-    
     location: {
         type: String
     },
+    latitude:{
+        type: String
+    },
+    longitude:{
+        type: String
+    }
   });
 
 module.exports = mongoose.model('Location', locationSchema);
--- server/src/public/map.js
+++ server/src/public/map.js
@@ -2,14 +2,14 @@
 let map;
 
 async function initMap() {
-  // The location of Uluru
+  // The location of cds
   const position = { lat: 42.349933,lng: -71.102930};
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  // The map, centered at cds
   map = new Map(document.getElementById("map"), {
     zoom: 13,
     center: position,
