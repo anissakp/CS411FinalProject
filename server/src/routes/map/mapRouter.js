8d774157e24acc47500e42f8f2239c32d4a63c77
Ana <anasof@bu.edu>
2023-12-04 15:44:42 -0500

    "locations from databaseeeee!!!!"

--- server/src/routes/map/mapRouter.js
+++ server/src/routes/map/mapRouter.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const locationModel = require('../../models/locationsModel');
 
 // Route to serve the map.html file
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public', 'map.html'));
+  res.sendFile(path.join(__dirname, '../../public', 'map.html'));
+});
+
+// Route to get all locations
+router.get('/locations', async (req, res) => {
+  try {
+    const locationData = await locationModel.find();
+    res.json(locationData);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
 });
 
 module.exports = router;
