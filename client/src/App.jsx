a0b7abe0f94f1b3c4b1f8ec6b186a98128a224b8
Ana <anasof@bu.edu>
2023-12-05 18:55:45 -0500

    "pushing map page"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './assets/HomePage';
+import MapPage from './assets/mapPage';
 
 const App = () => {
   return (
@@ -8,6 +9,8 @@ const App = () => {
       <div className="app-container">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/map" element={<MapPage />} /> {/* Add this route for the MapPage */}
+         
           {/* Add other routes as needed */}
         </Routes>
       </div>
--- client/src/assets/mapPage.jsx
+++ client/src/assets/mapPage.jsx
@@ -1,31 +1,11 @@
 import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
 import './mapStyle.css';
-import Map from '../../../server/src/public/map'; // Adjust the path based on your project structure
 
 const MapPage = () => {
-  useEffect(() => {
-    // Load additional scripts if needed
-    // const additionalScript = document.createElement('script');
-    // additionalScript.src = 'path/to/your/additional/script.js';
-    // additionalScript.type = 'text/javascript';
-    // additionalScript.async = true;
-    // document.head.appendChild(additionalScript);
-
-    // Cleanup if needed
-    // return () => {
-    //   document.head.removeChild(additionalScript);
-    // };
-  }, []);
 
   return (
     <body>
-      <Helmet>
-        <title>Add Map</title>
-        <link rel="stylesheet" type="text/css" href="./style.css" />
-        {/* Add other head elements like meta tags, links, etc. */}
-      </Helmet>
-
+ 
       <div className="header">
         <h1>GeoGrooves</h1>
         <div className="links">
@@ -43,12 +23,7 @@ const MapPage = () => {
           <p>Or create a new location by clicking the Create button</p>
         </div>
       </div>
-      <div className="mapContainer">
-        <div id="map">
-          {/* Embed your map component here */}
-          <Map />
-        </div>
-      </div>
+  
       <div className="NewLocation">
         <div className="textBox2">
           <h2>can't find it?</h2>
