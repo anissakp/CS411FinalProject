5eabda2f029f8a681b49434b720cbd5c705aad43
Raghu Nema <rnema@bu.edu>
2023-12-05 18:22:47 -0500

    locationPage stuff

--- client/src/assets/LocationPage.jsx
+++ client/src/assets/LocationPage.jsx
@@ -1,36 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const SearchSongBox = () => {
-    const [searchInput, setSearchInput] = useState('');
-
-    const handleChange = (event) => {
-        setInputValue(event.target.value);
-      };
-
-      return (
-        <div>
-          <label htmlFor="userInput">Enter something:</label>
-          <input
-            type="text"
-            id="userInput"
-            value={inputValue}
-            onChange={handleChange}
-            placeholder="Type here..."
-          />
-          <p>You typed: {inputValue}</p>
-        </div>
-      );
-    };
-
-export default SearchSongBox
-
-
-
-
-
 const locationPage = () => {
     const [playlistData, setPlaylistData] = useState(null);
+    const [searchInput, setSearchInput] = useState('');
+    const [searchInfo, setSearchInfo] = useState('');
 
     //how do i get the playlistId
     const playlistId = "12123";
@@ -47,9 +21,49 @@ const locationPage = () => {
         fetchPlaylist();
     }, [playlistId]);
    
+    const handleChange = async (event) => {
+        setSearchInput(event.target.value);
+        try {
+            const searchInfoResponse = await axios.get(`http://localhost:3000/spotify/search-playlist?playlistId=${searchInput}`)
+            setSearchInfo(searchInfoResponse);
+        } catch (error) {
+            console.error('Error fetching playlist:', error);
+        }
+    };
 
-    //search button to search for a song
-    //get the data and display it well
+    return (
+        <div>
+          <label htmlFor="userInput">Enter something:</label>
+          <input
+            type="text"
+            id="userInput"
+            value={searchInput}
+            onChange={handleChange}
+            placeholder="Type here..."
+          />
+          <p>You typed: {searchInput}</p>
 
-    //add song to track using the spotify(addTrack)
+        {playlistData ? (
+            <div>
+            <h1>{playlistData.name}</h1>
+            <p>{playlistData.description}</p>
+            {/* Additional rendering for tracks, images, etc. */}
+            </div>
+            ) : (
+            <p>Loading...</p>
+            )}
+            
+        {searchInfo ? (
+            <div>
+                <h1>{searchInfo.name}</h1>
+                <p>{searchInfo.description}</p>
+                {/* Additional rendering for tracks, images, etc. */}
+            </div>
+            ) : (
+            <p>Loading...</p>
+            )}
+        </div>
+    );
 }
+
+export default locationPage;
