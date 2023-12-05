8845132f5b74a6ab7837b8db0364a26d8e27c6da
Ana <anasof@bu.edu>
2023-12-04 22:03:51 -0500

    "map page+get playlistID by clicking marker"

--- server/src/public/map.js
+++ server/src/public/map.js
@@ -1,6 +1,7 @@
 // Initialize and add the map
 let map;
 
+
 async function initMap() {
   // Request needed libraries.
   //@ts-ignore
@@ -14,18 +15,47 @@ async function initMap() {
     mapId: "DEMO_MAP_ID",
   });
 
-  // Fetch locations from the server
-  const response = await fetch('/map/locations');
-  const locationsData = await response.json();
+// Fetch locations from the server
+const response = await fetch('/map/locations');
+const locationsData = await response.json();
 
-  // Add markers for each location
-  locationsData.forEach(location => {
+console.log(locationsData);  // Log the fetched data
+// Add markers for each location
+locationsData.forEach(location => {
+  // Parse latitude and longitude as numbers
+  const lat = parseFloat(location.latitude);
+  const lng = parseFloat(location.longitude);
+
+  // Check if the values are valid numbers
+  if (!isNaN(lat) && !isNaN(lng)) {
     const marker = new AdvancedMarkerElement({
       map: map,
-      position: { lat: parseFloat(location.latitude), lng: parseFloat(location.longitude) },
+      position: { lat, lng },
       title: location.name,
     });
-  });
+    // Check if playlistID exists in the location object
+    if ('playlistID' in location) {
+      // Add playlistID to the marker
+      marker.playlistID = location.playlistID;
+
+      // Add click event listener to the marker
+      marker.addListener('click', () => {
+        // Access the playlistID when the marker is clicked
+        console.log('Clicked on marker:', marker.title, marker.playlistID);
+        // You can now use marker.playlistID as needed
+      });}
+
+ 
+
+  } else {
+    console.error(`Invalid latitude or longitude for location: ${location.name}`);
+  }
+  
+});
+
+
+
 }
+
 
 initMap();
--- server/src/routes/map/mapRouter.js
+++ server/src/routes/map/mapRouter.js
@@ -19,4 +19,6 @@ router.get('/locations', async (req, res) => {
   }
 });
 
+
+
 module.exports = router;
