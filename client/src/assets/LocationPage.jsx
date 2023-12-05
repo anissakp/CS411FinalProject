f26f939463607e165c604d49c362b6ed2d9aaa2f
Raghu Nema <rnema@bu.edu>
2023-12-05 17:27:00 -0500

    stuff

--- client/src/assets/LocationPage.jsx
+++ client/src/assets/LocationPage.jsx
@@ -4,10 +4,14 @@ import axios from 'axios';
 const locationPage = () => {
     //get the location id
     //get the playlist from spotify(getPlaylist)
+    const playlistInfo = axios.get("http://localhost:3000/spotify/search-track", {
+        body: {
+            track: "userInput" //this will be the user input
+        }
+    })
 
     //search button to search for a song
     //get the data and display it well
 
     //add song to track using the spotify(addTrack)
-
 }
