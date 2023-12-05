30f72b6e4e76bafbe3d7de7da3119a1647b63473
Anissa Patel <anissakp@bu.edu>
2023-12-05 18:39:26 -0500

    i made the button pretty

--- client/src/assets/HomePage.jsx
+++ client/src/assets/HomePage.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { useHistory } from 'react-router-dom';
+const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh', 
+    width: 1400
+  };
 
+  const buttonStyle = {
+    padding: '15px 30px',
+    backgroundColor: 'green',
+    color: 'white',
+    borderRadius: '20px',
+    textDecoration: 'none',
+    fontSize: '18px',
+  };
 const HomePage = () => {
-    
-    const containerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh', // Adjust the height as needed
-      };
-
+  
     return (
     <div style={containerStyle} >
-      <h1>Connect to Spotify</h1>
-      <Link to="http://localhost:3000/spotify/login">Log In</Link>
+      <h1>Welcome to GeoGrooves</h1>
+      <h2>Connect to Spotify</h2>
+      <Link to="http://localhost:3000/spotify/login" style={buttonStyle} >Log In</Link>
     </div>
   );
 };
 
 export default HomePage;
+
