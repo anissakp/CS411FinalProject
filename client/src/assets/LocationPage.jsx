ec9286a84df274e37eab10f273de4da0ca43a399
Raghu Nema <rnema@bu.edu>
2023-12-05 16:56:02 -0500

    location page in process

---
+++ client/src/assets/LocationPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const locationPage = () => {
+    //get the location id
+    //get the playlist from spotify(getPlaylist)
+
+    //search button to search for a song
+    //get the data and display it well
+
+    //add song to track using the spotify(addTrack)
+}
