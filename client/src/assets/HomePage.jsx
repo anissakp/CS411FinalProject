312584648e3f33d073fb1935e937f6c1235fc714
Anissa Patel <anissakp@bu.edu>
2023-12-05 17:42:00 -0500

    locationpage

--- client/src/assets/HomePage.jsx
+++ client/src/assets/HomePage.jsx
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom';
 // import { useHistory } from 'react-router-dom';
 
 const HomePage = () => {
-//   const history = useHistory();
-  
-//   const handleButtonClick = () => {
-//     // Redirect to http://localhost:3000/spotify/login
-//     history.push('http://localhost:3000/spotify/login');
-//   };
+    
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh', // Adjust the height as needed
+      };
 
-  return (
-    <div>
+    return (
+    <div style={containerStyle} >
       <h1>Connect to Spotify</h1>
-      {/* <button onClick={handleButtonClick}>Connect to Spotify</button>  */}
-      {/* < button href='http://localhost:3000/spotify/login' > Log In </button> */}
       <Link to="http://localhost:3000/spotify/login">Log In</Link>
-
-
     </div>
   );
 };
--- client/src/assets/LocationPage.jsx
+++ client/src/assets/LocationPage.jsx
@@ -1,6 +1,34 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+const SearchSongBox = () => {
+    const [searchInput, setSearchInput] = useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+      };
+
+      return (
+        <div>
+          <label htmlFor="userInput">Enter something:</label>
+          <input
+            type="text"
+            id="userInput"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Type here..."
+          />
+          <p>You typed: {inputValue}</p>
+        </div>
+      );
+    };
+
+export default SearchSongBox
+
+
+
+
+
 const locationPage = () => {
     //get the location id
     //get the playlist from spotify(getPlaylist)
