fae37f63b72a38450477f538784486e2c9bb3885
Raghu Nema <rnema@bu.edu>
2023-12-05 17:42:57 -0500

    get playlist v1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import MapPage from '../src/assets/mapPage';
-import './App.css';
-
-const App = () => {
-  return (
-    <div className="app-container">
-      <MapPage />
-    </div>
-=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './assets/HomePage';
 
@@ -22,7 +12,6 @@ const App = () => {
         </Routes>
       </div>
     </Router>
->>>>>>> 16db76e4e39f9e01771f26fd2da883fb1cf718c4
   );
 };
 
--- client/src/assets/LocationPage.jsx
+++ client/src/assets/LocationPage.jsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const locationPage = () => {
-    //get the location id
-    //get the playlist from spotify(getPlaylist)
-    const playlistInfo = axios.get("http://localhost:3000/spotify/search-track", {
-        body: {
-            track: "userInput" //this will be the user input
+    const [playlistData, setPlaylistData] = useState(null);
+
+    //how do i get the playlistId
+    const playlistId = "12123";
+
+    useEffect(() => {
+        const fetchPlaylist = async () => {
+            try {
+                const playlistInfo = axios.get(`http://localhost:3000/spotify/search-playlist?playlistId=${playlistId}`)
+                setPlaylistData(playlistInfo.data);
+            } catch (error) {
+                console.error('Error fetching playlist:', error);
+            }
         }
-    })
+        fetchPlaylist();
+    }, [playlistId]);
+   
 
     //search button to search for a song
     //get the data and display it well
